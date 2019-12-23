15699f4cd679668fe8d0e53c0cd1474f7af46496
SPoket <lol8funny@gmail.com>
2019-12-23 21:21:36 +0400

    Remove 'main' prefixes in window name

--- src/c/main.c
+++ src/c/main.c
@@ -1,27 +1,27 @@
 #include <pebble.h>
 
-static Window *s_main_window;
+static Window *s_window;
 
-static void main_window_load(Window *window) {
+static void window_load(Window *window) {
 
 }
 
-static void main_window_unload(Window *window) {
+static void window_unload(Window *window) {
 }
 
 static void init() {
-  s_main_window = window_create();
+  s_window = window_create();
 
   window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
+    .load = window_load,
+    .unload = window_unload
   });
 
   window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
-  window_destroy(s_main_window);
+  window_destroy(s_window);
 }
 
 int main(void) {
