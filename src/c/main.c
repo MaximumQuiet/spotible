44f28df443065ac96840bc7a660ff3a5d6daaa42
SPoket <lol8funny@gmail.com>
2019-12-23 21:12:34 +0400

    Add main module - with init/deinit and main window

---
+++ src/c/main.c
@@ -0,0 +1,31 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+
+static void main_window_load(Window *window) {
+
+}
+
+static void main_window_unload(Window *window) {
+}
+
+static void init() {
+  s_main_window = window_create();
+
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
