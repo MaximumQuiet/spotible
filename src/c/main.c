ef2438d55bd3cc52d0e51b425b75bae4ddeb093a
SPoket <lol8funny@gmail.com>
2019-12-25 20:54:48 +0400

    Show 'pause' button on action bar

--- src/c/main.c
+++ src/c/main.c
@@ -9,6 +9,7 @@ static Window *s_window;
 static ActionBarLayer *s_action_bar;
 static GBitmap *s_icon_next;
 static GBitmap *s_icon_prev;
+static GBitmap *s_icon_pause;
 
 static void action_next_click_handler() {
 }
@@ -27,12 +28,14 @@ static void click_config_provider(void *context) {
 static void window_load(Window *window) {
   s_icon_next = gbitmap_create_with_resource(RESOURCE_ID_ICON_MUSIC_SKIP_FORWARD);
   s_icon_prev = gbitmap_create_with_resource(RESOURCE_ID_ICON_MUSIC_SKIP_BACKWARD);
+  s_icon_pause = gbitmap_create_with_resource(RESOURCE_ID_ICON_MUSIC_PAUSE);
 
   s_action_bar = action_bar_layer_create();
   action_bar_layer_add_to_window(s_action_bar, window);
   action_bar_layer_set_click_config_provider(s_action_bar, click_config_provider);
 
   action_bar_layer_set_icon(s_action_bar, BUTTON_ID_UP, s_icon_prev);
+  action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_icon_pause);
   action_bar_layer_set_icon(s_action_bar, BUTTON_ID_DOWN, s_icon_next);
 }
 
@@ -41,7 +44,9 @@ static void window_load(Window *window) {
  */
 static void window_unload(Window *window) {
   gbitmap_destroy(s_icon_next);
+  gbitmap_destroy(s_icon_next);
   gbitmap_destroy(s_icon_prev);
+
   action_bar_layer_destroy(s_action_bar);
 }
 
